4bde75ae85388f241e70d05ee231da9b561d800f
Tigran TIKSN Torosyan <personal@tiksn.com>
2019-09-21 02:00:22 +0300

    Create CachedConnectionProvider.cs

---
+++ src/CSharp/Providers/CachedConnectionProvider.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.Configuration;
+using RabbitMQ.Client;
+using System;
+
+namespace TIKSN.Lionize.Messaging.Providers
+{
+    public class CachedConnectionProvider
+    {
+        private readonly IConnectionFactory _connectionFactory;
+        private readonly object _connectionLocker;
+
+        public CachedConnectionProvider(IConfigurationRoot configurationRoot)
+        {
+            _connectionLocker = new object();
+
+            ConnectionFactory factory = new ConnectionFactory
+            {
+                Uri = new Uri(configurationRoot.GetConnectionString("RabbitMQ"))
+            };
+
+            _connectionFactory = factory;
+        }
+
+        public CachedConnection GetConnection()
+        {
+        }
+    }
+}

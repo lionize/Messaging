f1e7bc47c3a7e4282eedd8ee9d672ff089e059d0
Tigran TIKSN Torosyan <personal@tiksn.com>
2019-09-21 01:56:09 +0300

    Create CachedConnection.cs

---
+++ src/CSharp/Providers/CachedConnection.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.Logging;
+using RabbitMQ.Client;
+using System;
+
+namespace TIKSN.Lionize.Messaging.Providers
+{
+    public class CachedConnection : IDisposable
+    {
+        private readonly ILogger _logger;
+
+        public CachedConnection(IConnection connection, ILogger logger)
+        {
+            Connection = connection ?? throw new ArgumentNullException(nameof(connection));
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        }
+
+        public IConnection Connection { get; }
+
+        public void Dispose()
+        {
+            Connection.Dispose();
+            _logger.LogInformation("Connection disposed");
+        }
+    }
+}

123e4a6994fc3a8ae195309fa55c980d42137218
Tigran TIKSN Torosyan <personal@tiksn.com>
2019-09-21 02:11:43 +0300

    Update CachedConnection.cs

--- src/CSharp/Providers/CachedConnection.cs
+++ src/CSharp/Providers/CachedConnection.cs
@@ -6,20 +6,28 @@ namespace TIKSN.Lionize.Messaging.Providers
 {
     public class CachedConnection : IDisposable
     {
+        private readonly IDisposable _disposable;
         private readonly ILogger _logger;
 
-        public CachedConnection(IConnection connection, ILogger logger)
+        public CachedConnection(IConnection connection, IDisposable disposable, ILogger logger)
         {
             Connection = connection ?? throw new ArgumentNullException(nameof(connection));
+            _disposable = disposable ?? throw new ArgumentNullException(nameof(disposable));
             _logger = logger ?? throw new ArgumentNullException(nameof(logger));
         }
 
+        /// <summary>
+        /// Gets the connection.
+        /// </summary>
+        /// <value>
+        /// The connection.
+        /// </value>
+        /// <remarks>Do not dispose explicitly</remarks>
         public IConnection Connection { get; }
 
         public void Dispose()
         {
-            Connection.Dispose();
-            _logger.LogInformation("Connection disposed");
+            _disposable.Dispose();
         }
     }
 }

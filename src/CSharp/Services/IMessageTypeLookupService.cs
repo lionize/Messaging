be36aded154323143c5b75ba9293071b7c5e2727
Tigran TIKSN Torosyan <personal@tiksn.com>
2019-09-21 02:55:59 +0300

    Update IMessageTypeLookupService.cs

--- src/CSharp/Services/IMessageTypeLookupService.cs
+++ src/CSharp/Services/IMessageTypeLookupService.cs
@@ -5,7 +5,13 @@ namespace TIKSN.Lionize.Messaging.Services
     public interface IMessageTypeLookupService
     {
         string GetMessageName(Type type);
+
         string GetMessageName<TMessage>();
+
+        string GetMessageQueue(Type type);
+
+        string GetMessageQueue<TMessage>();
+
         Type GetMessageType(string name);
     }
 }

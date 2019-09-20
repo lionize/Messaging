9d6ccc4a621d23da481df8f157a53b01687e4a5f
Tigran TIKSN Torosyan <personal@tiksn.com>
2019-09-21 01:27:41 +0300

    Create ConsumerBackgroundService.cs

---
+++ src/CSharp/BackgroundServices/ConsumerBackgroundService.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Options;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using TIKSN.Lionize.Messaging.Options;
+
+namespace TIKSN.Lionize.Messaging.BackgroundServices
+{
+    public class ConsumerBackgroundService<TMessage> : BackgroundService
+    {
+        private readonly IOptions<ApplicationOptions> _applicationOptions;
+
+        public ConsumerBackgroundService(IOptions<ApplicationOptions> applicationOptions)
+        {
+            _applicationOptions = applicationOptions ?? throw new ArgumentNullException(nameof(applicationOptions));
+        }
+
+        protected override Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

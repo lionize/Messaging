99128ed46e6f0918550fec79ec20059fc2e48474
Tigran TIKSN Torosyan <personal@tiksn.com>
2019-09-21 02:21:29 +0300

    Create IMessageTypeLookupService.cs

---
+++ src/CSharp/Services/IMessageTypeLookupService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace TIKSN.Lionize.Messaging.Services
+{
+    public interface IMessageTypeLookupService
+    {
+        string GetMessageName(Type type);
+        Type GetMessageType(string name);
+    }
+}

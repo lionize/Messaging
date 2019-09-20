e23ca34c96866d5fcc00d5f4056b6c83e5eb3d5e
Tigran TIKSN Torosyan <personal@tiksn.com>
2019-09-21 02:36:49 +0300

    Create IConsumerMessageHandler.cs

---
+++ src/CSharp/Handlers/IConsumerMessageHandler.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using TIKSN.Integration.Correlation;
+
+namespace TIKSN.Lionize.Messaging.Handlers
+{
+    public interface IConsumerMessageHandler<TMessage>
+    {
+        Task HandleAsync(TMessage message, CorrelationID correlationID, CancellationToken cancellationToken);
+    }
+}

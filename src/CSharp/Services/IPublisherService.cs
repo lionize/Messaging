c1bb00e374ea46400613d83709aef51afdd42a57
Tigran TIKSN Torosyan <personal@tiksn.com>
2019-09-21 03:16:05 +0300

    Create IPublisherService.cs

---
+++ src/CSharp/Services/IPublisherService.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using TIKSN.Integration.Correlation;
+
+namespace TIKSN.Lionize.Messaging.Services
+{
+    public interface IPublisherService
+    {
+        Task ProduceAsync<TMessage>(TMessage message, CorrelationID correlationID, CancellationToken cancellationToken);
+    }
+}

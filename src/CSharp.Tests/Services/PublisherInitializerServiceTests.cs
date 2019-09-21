e2bbae9b7e1d16b63504f5346e62e3d78d6bbec6
Tigran TIKSN Torosyan <personal@tiksn.com>
2019-09-21 12:17:05 +0300

    Create PublisherInitializerServiceTests.cs

---
+++ src/CSharp.Tests/Services/PublisherInitializerServiceTests.cs
@@ -0,0 +1,31 @@
+﻿using Autofac;
+using Autofac.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
+using System.Threading.Tasks;
+using TIKSN.DependencyInjection;
+using Xunit;
+
+namespace TIKSN.Lionize.Messaging.Services.Tests
+{
+    public class PublisherInitializerServiceTests
+    {
+        private readonly IPublisherInitializerService _publisherInitializerService;
+
+        public PublisherInitializerServiceTests()
+        {
+            var services = new ServiceCollection();
+            services.AddFrameworkPlatform();
+            var builder = new ContainerBuilder();
+            builder.RegisterModule<MessagingAutofacModule>();
+            builder.Populate(services);
+            var serviceProvider = new AutofacServiceProvider(builder.Build());
+            _publisherInitializerService = serviceProvider.GetRequiredService<IPublisherInitializerService>();
+        }
+
+        [Fact]
+        public async Task InitializeTest()
+        {
+            await _publisherInitializerService.InitializeAsync(default);
+        }
+    }
+}
